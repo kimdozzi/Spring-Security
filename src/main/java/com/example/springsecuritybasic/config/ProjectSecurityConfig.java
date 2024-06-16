2b2ed687b20a940d4d155883c5b9fc21de6b53eb
kimdozzi <neo5188@gmail.com>
2024-06-16 17:27:04 +0900

    jdbcUserDetailsManager를 사용한 인증
    - 기존의 InMemoryUserDetailsManager 방식 미사용
    - JdbcUserDetailsManager 사용을 위한 Bean 등록

--- src/main/java/com/example/springsecuritybasic/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -1,14 +1,14 @@
 package com.example.springsecuritybasic.config;
 
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,19 +26,23 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails admin = User.withUsername("admin")
-                .password("123")
-                .authorities("admin")
-                .build();
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        UserDetails admin = User.withUsername("admin")
+//                .password("123")
+//                .authorities("admin")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin);
+//    }
 
-        return new InMemoryUserDetailsManager(admin);
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() { // Spring Security에게 비밀번호를 어떻게 저장하였는지 알려주는 역할
         return NoOpPasswordEncoder.getInstance();
     }
-
 }
